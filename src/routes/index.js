060ba78bd2a2478f37255eb704e3466eec380256
git-k <kgunu93@gmail.com>
2020-11-09 20:21:22 +0900

    / 접근시 자동으로 /login 으로 가도록 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,22 +2,33 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../view/Login";
 import NotFound from "../view/NotFound";
+import Home from "../view/Home";
 
 Vue.use(VueRouter);
+
+const isAuth = (req, res, next) => {
+  console.log(`isAuth`);
+  next("/login");
+};
 
 const router = new VueRouter({
   mode: "history",
   routes: [
     {
+      path: "/",
+      component: Home,
+      beforeEnter: isAuth,
+    },
+    {
       path: "/login",
-      component: Login
+      component: Login,
       //beforeEnter: isAuth
     },
     {
       path: "*",
-      component: NotFound
-    }
-  ]
+      component: NotFound,
+    },
+  ],
 });
 
 export default router;
